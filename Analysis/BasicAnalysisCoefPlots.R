69d364fa43f633c8cb2d2a7c67bdf40c69d40d2a
Christopher Gandrud <christopher.gandrud@gmail.com>
2012-07-09 10:03:05 +0200

    add analysis only source file

--- Analysis/BasicAnalysisCoefPlots.R
+++ Analysis/BasicAnalysisCoefPlots.R
@@ -4,35 +4,6 @@
 # 7 July 2012
 ####################
 
-library(MatchIt)
-library(Zelig)
-library(stats)
-
-
-# Subset for complete (nonmissing) variables
-vars <- c("ElectionPeriod", "pres_party", "error.prop.deflator.q2", "time_to_election", "recession", "senate_dem_rep", "house_dem_rep", "DebtGDP", "ExpenditureGDP", "PotentialGDP")  
-cpi.complete <- cpi.data[complete.cases(cpi.data[vars]),]
-cpi.complete <- cpi.complete[vars]
-
-#### Matching Model ####
-
-# Party 
-cpi.matched.party <- matchit(pres_party ~ recession + time_to_election + senate_dem_rep + house_dem_rep + ExpenditureGDP + PotentialGDP, data = cpi.complete, method = "genetic")
-
-# Elections
-# The matching model diagnostic statistics showed that the matched data set was not balanced, this was probably because there were too few election quarters.
-cpi.matched.election <- matchit(ElectionPeriod ~ recession + senate_dem_rep + house_dem_rep + ExpenditureGDP + PotentialGDP, data = cpi.complete, method = "genetic")
-
-# Diagnostics for Covariate Balance
-# summary(cpi.matched.party)
-# plot(cpi.matched.party)
-
-# summary(cpi.matched.election)
-# plot(cpi.matched.election)
-
-# Turn matched data into data.frame for analysis
-cpi.Mdf.party <- match.data(cpi.matched.party)
-
 #### Presidential Party Models ####
 
 #### Non-matched (NP) ####
