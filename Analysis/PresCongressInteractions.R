e4efabbf46551ba524db9f319f4e90299f273def
Christopher Gandrud <christopher.gandrud@gmail.com>
2012-07-19 16:58:40 +0200

    new interaction plot

---
+++ Analysis/PresCongressInteractions.R
@@ -0,0 +1,68 @@
+################
+# President*Congress Interaction Graphs
+# Christopher Gandrud
+# Updated 19 July 2012 
+################
+
+#### House ####
+
+# Create range of values to simulate expected values across
+pres_party.r <- c(0, 1)
+house_dem_rep.r <- seq(0.87, 2.04, by = .01)
+
+# Set fitted values 
+PL10SetDem <- setx(PL10, pres_party = pres_party.r, house_dem_rep = 1.3, senate_dem_rep = 1.2)
+PL10SetRep <- setx(PL10, pres_party = pres_party.r, house_dem_rep = 0.9, senate_dem_rep = 0.9)
+
+# Simulate expected values.
+PL10SimDem <- sim(PL10, x = PL10.SetDem)
+PL10SimRep <- sim(PL10, x = PL10.SetRep)
+
+# Extract expected values from simulations (Dem)
+PL10SimDem.ev <- PL10SimDem$qi
+PL10SimDem.ev <-data.frame(PL10SimDem.ev$ev)
+PL10SimDem.ev <- melt(PL10SimDem.ev, measure = 1:2)
+
+# Remove 'X' from variable
+PL10SimDem.ev$variable <- as.numeric(gsub("X", "", PL10SimDem.ev$variable))
+
+# Extract expected values from simulations (Rep)
+PL10SimRep.ev <- PL10SimRep$qi
+PL10SimRep.ev <-data.frame(PL10SimRep.ev$ev)
+PL10SimRep.ev <- melt(PL10SimRep.ev, measure = 1:2)
+
+# Remove 'X' from variable
+PL10SimRep.ev$variable <- as.numeric(gsub("X", "", PL10SimRep.ev$variable))
+
+# Replace nonsense numbers with simulation names
+PL10SimDem.ev$variable[PL10SimDem.ev$variable == 17] <- "Republican President"
+PL10SimDem.ev$variable[PL10SimDem.ev$variable == 22] <- "Democratic President"
+PL10SimDem.ev$variable <- factor(PL10SimDem.ev$variable)
+PL10SimDem.ev$Congress <- "Dem. Congress"
+
+PL10SimRep.ev$variable[PL10SimRep.ev$variable == 17] <- "Republican President"
+PL10SimRep.ev$variable[PL10SimRep.ev$variable == 22] <- "Democratic President"
+PL10SimRep.ev$variable <- factor(PL10SimRep.ev$variable)
+PL10SimRep.ev$Congress <- "Rep. Congress"
+
+# Append both sets of simulation results
+PL10Bound <- rbind(PL10SimRep.ev, PL10SimDem.ev)
+
+#### Create plots ####
+
+# Partisan colours
+partisan.congress.colours = c("Rep. Congress" = "#C42B00", "Dem. Congress" = "#2259B3")
+
+PartyInteractionPlot <- ggplot(data = PL10Bound, aes(variable, value)) +
+                                  geom_hline(aes(intercept= 0), linetype = "dotted") +
+                                  stat_summary(fun.y = mean, geom = "line", aes(group = Congress), colour = "grey70") +
+                                  geom_point(shape = 21, aes(color = Congress), alpha = I(0.05), size = 7) +
+                                  scale_x_discrete(limits = c("Republican President", "Democratic President")) +
+                                  xlab("") + ylab("Expected Standardized Forecast Error\n") +
+                                  scale_color_manual(values = partisan.congress.colours, name = "") +
+                                  guides(colour = guide_legend(override.aes = list(alpha = 1))) +
+                                  theme_bw()
+                                  
+print(PartyInteractionPlot)
+
+
