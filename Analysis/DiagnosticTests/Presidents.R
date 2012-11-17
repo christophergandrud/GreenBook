5b7459f40c21eac60f3905320a14fda5d0a69c21
Christopher Gandrud <christopher.gandrud@gmail.com>
2012-11-17 16:11:42 +0900

    pres diagnostics

---
+++ Analysis/DiagnosticTests/Presidents.R
@@ -0,0 +1,58 @@
+################
+# GreenBook: President Diagnostics
+# Christopher Gandrud
+# 17 November 2012
+###############
+
+## Load libraries
+# library(devtools)
+library(Zelig)
+library(stringr)
+# library(plyr)
+
+# To run as a stand alone file. First, run the following files from earlier in the paper:
+## source_url("http://bit.ly/NXdCpk") 
+
+# Subset for complete (nonmissing) values
+# matchit requires data sets to have no missing values
+vars <- c("Quarter", "ElectionPeriod", "pres_party", "error.prop.deflator.q2", 
+          "time_to_election", "recession", "senate_dem_rep", 
+          "house_dem_rep", "DebtGDP", "ExpenditureGDP", "UNRATE",
+          "PotentialGDP", "GlobalModel", "FedFunds", "FedFunds2qChange", "DiscountRate",
+          "DiscountRate2qChange", "Chair"
+)  
+cpi.complete <- cpi.data[complete.cases(cpi.data[vars]),]
+cpi.complete <- cpi.complete[vars]
+
+cpi.complete <- subset(cpi.complete, !(time_to_election %in% c(14, 15)))
+
+cpi.data2 <- subset(cpi.data, !(time_to_election %in% c(14, 15)))
+
+cpi.data2$elect2 <- (cpi.data2$time_to_election)^2
+
+cpi.data2$elect2 <- factor(cpi.data2$president)
+
+#### Run regressions dropping each president ####
+# Remove spaces in president's names
+cpi.data2$president <- str_replace_all(cpi.data2$president, " ", "")
+cpi.data2$elect2 <- factor(cpi.data2$president)
+
+# Create funciton and run regressions
+PresNames <- c("Nixon", "Ford", "Carter", "Reagan", "GHWBush", "Clinton", "GWBush")
+
+SubSetPres <- function(x){
+            SubData <- subset(cpi.data2, president != x)
+            SData <- paste("S", x, sep = "")
+            assign(SData, summary(zelig(error.prop.deflator.q2 ~ pres_party + 
+                                        time_to_election + recession + senate_dem_rep +
+                                        house_dem_rep + DebtGDP + ExpenditureGDP + 
+                                        PotentialGDP + DiscountRate2qChange + GlobalModel +
+                                        UNRATE, model = "ls", data = SubData, 
+                                        cite = FALSE)), 
+                   env=.GlobalEnv)
+}
+
+lapply(PresNames, SubSetPres)
+
+#### President Dummy Variables ####
+MPresDummies <- zelig(error.prop.deflator.q2 ~ time_to_election + recession + senate_dem_rep + house_dem_rep + DebtGDP + ExpenditureGDP + PotentialGDP + DiscountRate2qChange + GlobalModel + UNRATE + president, model = "ls", data = cpi.data2, cite = FALSE)
