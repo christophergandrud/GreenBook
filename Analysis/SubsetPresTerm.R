5fd9ae41dc830f5ed521851b6e543c33c0e5d5f7
Christopher Gandrud <christopher.gandrud@gmail.com>
2012-11-23 11:34:08 +0900

    pres update

--- Analysis/SubsetPresTerm.R
+++ Analysis/SubsetPresTerm.R
@@ -7,17 +7,16 @@
 # library(Zelig)
 
 # Create funciton and run regressions
-PresTerms <- c("Nixon1", "Nixon2", "Ford1", "Carter1", "Reagan1", "Reagan2", "GHWBush1", "Clinton1", "Clinton2", "GWBush1", "GWBush2")
+PresTerms <- c("Nixon1", "Nixon2", "Ford1", "Carter1", "Reagan1", "Reagan2", 
+                "GHWBush1", "Clinton1", "Clinton2", "GWBush1", "GWBush2"
+                )
 
 SubSetPresTerms <- function(x){
   SubData <- subset(cpi.data2, presTerm != x)
   SData <- paste("S", x, sep = "")
-  assign(SData, summary(zelig(error.prop.deflator.q2 ~ pres_party + 
-                                time_to_election + recession + senate_dem_rep +
-                                house_dem_rep + DebtGDP + ExpenditureGDP + 
-                                PotentialGDP + DiscountRate2qChange + GlobalModel +
-                                UNRATE, model = "ls", data = SubData, 
-                              cite = FALSE)), 
+  assign(SData, zelig(error.prop.deflator.q2 ~ recession + DebtGDP + ExpenditureGDP +
+                        PotentialGDP + DiscountRate2qChange + UNRATE + time_to_election +
+                        pres_party, UNRATE, model = "ls", data = SubData, cite = FALSE), 
          env=.GlobalEnv)
 }
 
