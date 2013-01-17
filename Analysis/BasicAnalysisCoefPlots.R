6a6f5ddc839cb618422bcb1728e38406ee3df3f1
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-01-17 19:19:29 +0900

    Update coef plot

--- Analysis/BasicAnalysisCoefPlots.R
+++ Analysis/BasicAnalysisCoefPlots.R
@@ -1,115 +1,95 @@
 ####################
 # Greenbook MatchIt Analyses Coef Plots Compare
 # Christopher Gandrud
-# 29 November 2012
+# 17 January 2013
 ####################
 
-#### Figures for ls Results Catapilar Plot ####
-# .cat for catapilar graph
-# .sum for summary
-library(reshape)
+#### Figures for Results Catapilar Plot ####
+#### Create Function to find and melt coefficients ####
+CoefPlotPrep <- function(x){
+  require(ZeligBayesian)
+  
+  y <- substr(deparse(substitute(x)), start = 1, stop = 1) 
+  z <- substr(deparse(substitute(x)), start = 2, stop = 2)
+  n <- substr(deparse(substitute(x)), start = 3, stop = 3)
+  
+  # Find confidence bounds
+  if (z == "L"){
+    Conf <- confint(x)
+    Sum <- as.data.frame(Conf)
+  } else if (z == "B"){
+    Conf <- summary(x)
+    Sum <- as.data.frame(Conf$summary)
+  }
+  
+  Sum$var <- rownames(Sum)
+  
+  # Extract lower bounds
+  if (z == "L"){
+    Lower.molten <- reshape2::melt(Sum, id = c("var"), measure.vars = c("2.5 %"))
+  } else if (z == "B"){
+    Lower.molten <- reshape2::melt(Sum, id = c("var"), measure.vars = c("2.5%"))
+  }
+  Lower.molten <- reshape::rename(Lower.molten, c(value = "lower"))
+  Lower.molten <- Lower.molten[, -2]
+  
+  # Extract upper bounds
+  if (z == "L"){
+    Upper.molten <- reshape2::melt(Sum, id = c("var"), measure.vars = c("97.5 %"))
+  } else if (z == "B"){
+    Upper.molten <- reshape2::melt(Sum, id = c("var"), measure.vars = c("97.5%"))
+  }
+  Upper.molten <- reshape::rename(Upper.molten, c(value = "upper"))
+  Upper.molten <- Upper.molten[, -2]
+  
+  # Merge upper and lower bounds
+  Molten <- merge(Lower.molten, Upper.molten)
+  
+  # Add descriptors 
+  if (y == "N"){
+    Molten$match <- "Not Matched"
+  } else if (y == "P"){
+    Molten$match <- "Matched"
+  }
+  if (z == "L"){
+    Molten$method <- "Normal Linear"
+  } else if (z == "B"){
+    Molten$method <- "Normal Bayes"
+  } 
+  Name <- paste0(y, z, n, "Estimates")
+  assign(Name, Molten, envir = .GlobalEnv)
+}
 
-### LS 
-# Extract and melt quantiles for marginal posterior distributions (not matched data set)
-NL5.cat <- confint(NL5)
-NL5.cat.sum <- as.data.frame(NL5.cat)
-NL5.cat.sum$var <- rownames(NL5.cat.sum)
+#### Run function ####
+CoefPlotPrep(NL5)
+CoefPlotPrep(PL5)
+CoefPlotPrep(NB1)
+CoefPlotPrep(PB1)
 
-NL5.lower.molten <- melt(NL5.cat.sum, id = c("var"), measure.vars = c("2.5 %"))
-NL5.lower.molten <- rename(NL5.lower.molten, c(value = "lower"))
-NL5.lower.molten <- NL5.lower.molten[, -2]
-
-NL5.upper.molten <- melt(NL5.cat.sum, id = c("var"), measure.vars = c("97.5 %"))
-NL5.upper.molten <- rename(NL5.upper.molten, c(value = "upper"))
-NL5.upper.molten <- NL5.upper.molten[, -2]
-
-NL5.molten <- merge(NL5.lower.molten, NL5.upper.molten)
-NL5.molten$match <- "Not Matched"
-
-# Extract and melt quantiles for marginal posterior distributions (matched data set)
-PL5.cat <- confint(PL5)
-PL5.cat.sum <- as.data.frame(PL5.cat)
-PL5.cat.sum$var <- rownames(PL5.cat.sum)
-
-PL5.lower.molten <- melt(PL5.cat.sum, id = c("var"), measure.vars = c("2.5 %"))
-PL5.lower.molten <- rename(PL5.lower.molten, c(value = "lower"))
-PL5.lower.molten <- PL5.lower.molten[, -2]
-
-PL5.upper.molten <- melt(PL5.cat.sum, id = c("var"), measure.vars = c("97.5 %"))
-PL5.upper.molten <- rename(PL5.upper.molten, c(value = "upper"))
-PL5.upper.molten <- PL5.upper.molten[, -2]
-
-PL5.molten <- merge(PL5.lower.molten, PL5.upper.molten)
-PL5.molten$match <- "Matched"
-
-estimates.ls <- rbind(NL5.molten, PL5.molten)
-estimates.ls$method <- "Normal Linear"
-
-
-
-#### Figures for normal.bayes Results Catapilar Plot ####
-# .cat for catapilar graph
-# .sum for summary
-
-### Normal Bayes Models 
-# Extract and melt quantiles for marginal posterior distributions (not matched data set)
-NB1.cat <- summary(NB1)
-NB1.cat.sum <- as.data.frame(NB1.cat$summary)
-NB1.cat.sum$var <- rownames(NB1.cat.sum)
-
-NB1.lower.molten <- melt(NB1.cat.sum, id = c("var"), measure.vars = c("2.5%"))
-NB1.lower.molten <- rename(NB1.lower.molten, c(value = "lower"))
-NB1.lower.molten <- NB1.lower.molten[, -2]
-
-NB1.upper.molten <- melt(NB1.cat.sum, id = c("var"), measure.vars = c("97.5%"))
-NB1.upper.molten <- rename(NB1.upper.molten, c(value = "upper"))
-NB1.upper.molten <- NB1.upper.molten[, -2]
-
-NB1.molten <- merge(NB1.lower.molten, NB1.upper.molten)
-NB1.molten$match <- "Not Matched"
-
-# Extract and melt quantiles for marginal posterior distributions (matched data set)
-PB1.cat <- summary(PB1)
-PB1.cat.sum <- as.data.frame(PB1.cat$summary)
-PB1.cat.sum$var <- rownames(PB1.cat.sum)
-
-PB1.lower.molten <- melt(PB1.cat.sum, id = c("var"), measure.vars = c("2.5%"))
-PB1.lower.molten <- rename(PB1.lower.molten, c(value = "lower"))
-PB1.lower.molten <- PB1.lower.molten[, -2]
-
-PB1.upper.molten <- melt(PB1.cat.sum, id = c("var"), measure.vars = c("97.5%"))
-PB1.upper.molten <- rename(PB1.upper.molten, c(value = "upper"))
-PB1.upper.molten <- PB1.upper.molten[, -2]
-
-PB1.molten <- merge(PB1.lower.molten, PB1.upper.molten)
-PB1.molten$match <- "Matched"
-
-estimates.b <- rbind(NB1.molten, PB1.molten)
-estimates.b$method <- "Normal Bayes"
-
-estimates <- rbind(estimates.ls, estimates.b)
+# Bind into one data frame
+estimates <- rbind(NL5Estimates, PL5Estimates, NB1Estimates, PB1Estimates)
 
 # Remove Intercept
 estimates <- subset(estimates, var != c("(Intercept)"))
 estimates <- subset(estimates, var != c("sigma2"))
 
-
-##### Create comparison plot
-
+##### Create comparison plot ####
 cols <- c("#B35B40", "#696969")
+
 breaks <- c("pres_party", "ExpenditureGDP", "recession", "DebtGDP", "time_to_election", "PotentialGDP", "DiscountRate2qChange", "UNRATE", "GlobalModelAfter 1996")
+
 break.labels <- c("Dem. President", "Gov. Expenditure (% GDP)", "Recession", "Gov. Debt (% GDP)", "Quarters Until Election", "Output Gap", "Discount Rate Change", "Unemployment Rate", "Global Model")
 
 est.plot <- ggplot(data = estimates, aes(x = reorder(var, lower), ymin = lower, ymax = upper, colour = match)) +
-                      facet_grid(~ method) + 
-                      geom_linerange(size = 3, alpha = 0.6) +
-                      scale_x_discrete(breaks = breaks, labels = break.labels) +
-                      #scale_y_continuous(breaks = c(-1, 0.0, 0.5), labels = c("-1", "0", "0.5")) +
-                      scale_color_manual(values = cols, name = "") +                            
-                      geom_hline(aes(intercept= 0), linetype = "dotted") +
-                      ylab("\nCoefficient Estimate") + xlab("") +
-                      coord_flip() +
-                      theme_bw(base_size = 11)
+  facet_grid(~ method) + 
+  geom_linerange(size = 3, alpha = 0.6) +
+  scale_x_discrete(breaks = breaks, labels = break.labels) +
+  #scale_y_continuous(breaks = c(-1, 0.0, 0.5), labels = c("-1", "0", "0.5")) +
+  scale_color_manual(values = cols, name = "") +                            
+  geom_hline(aes(intercept= 0), linetype = "dotted") +
+  ylab("\nCoefficient Estimate") + xlab("") +
+  coord_flip() +
+  theme_bw(base_size = 11)
 
 print(est.plot)
 
