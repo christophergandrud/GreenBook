d8c5654af43d63f2119cee829bc54b1aea8cded6
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-10-13 14:26:58 +0200

    shock variables added

--- Analysis/SupplementaryAnalyses.R
+++ Analysis/SupplementaryAnalyses.R
@@ -1,7 +1,7 @@
 ###############
 # Supplemental Analyses for GreenBook Forecast Error Paper
 # Christopher Gandrud 
-# 12 October 2013
+# 13 October 2013
 ###############
 
 ## Load libraries
@@ -19,7 +19,7 @@ vars <- c("Quarter", "ElectionPeriod4", "pres_party", "error.prop.deflator.q2",
           "time_to_election", "recession", "senate_dem_rep", "cpi_change",
           "house_dem_rep", "DebtGDP", "DeficitGDP", "ExpenditureGDP", "UNRATE",
           "PotentialGDP", "GlobalModel", "FedFunds", "FedFunds2qChange", "DiscountRate",
-          "DiscountRate2qChange", "Chair"
+          "DiscountRate2qChange", "Chair", "productivity_change", "WTI_crude_price", "num_conflicts"
 )  
 cpi.complete <- cpi.data[complete.cases(cpi.data[vars]),]
 cpi.complete <- cpi.complete[vars]
@@ -31,7 +31,7 @@ cpi.complete <- subset(cpi.complete, !(time_to_election %in% c(14, 15)))
 cpi.data2 <- subset(cpi.data, !(time_to_election %in% c(14, 15)))
 
 # Create inflation lag variable
-cpi.data2 <- slide(cpi.data2, Var = "cpi_change", NewVar = "cpi_change3", slideBy = -3)
+cpi.data2 <- slide(cpi.data2, Var = "deflator", NewVar = "deflatorLag3", slideBy = -3)
 
 
 ###### Models #####
@@ -39,4 +39,17 @@ cpi.data2 <- slide(cpi.data2, Var = "cpi_change", NewVar = "cpi_change3", slideB
 # Pres. ID*Linear Time to Election
 S1 <- zelig(error.prop.deflator.q2 ~ recession + ExpenditureGDP + PotentialGDP + DiscountRate2qChange + UNRATE + pres_party*time_to_election + time_to_election + GlobalModel + senate_dem_rep + house_dem_rep, model = "normal", data = cpi.data2, cite = FALSE)
 
-S2 <- zelig(error.prop.deflator.q2 ~ recession + ExpenditureGDP + PotentialGDP + DiscountRate2qChange + UNRATE + pres_party + time_to_election + senate_dem_rep + house_dem_rep + cpi_change3, model = "normal", data = cpi.data2, cite = FALSE)
+# Present inflation
+S2 <- zelig(error.prop.deflator.q2 ~ recession + ExpenditureGDP + PotentialGDP + DiscountRate2qChange + UNRATE + pres_party + time_to_election + senate_dem_rep + house_dem_rep + deflator, model = "normal", data = cpi.data2, cite = FALSE)
+
+# Inflation in the quarter before the forecast quarter
+S3 <- zelig(error.prop.deflator.q2 ~ recession + ExpenditureGDP + PotentialGDP + DiscountRate2qChange + UNRATE + pres_party + time_to_election + senate_dem_rep + house_dem_rep + deflatorLag3, model = "normal", data = cpi.data2, cite = FALSE)
+
+# Oil Shocks
+S4 <- zelig(error.prop.deflator.q2 ~ recession + ExpenditureGDP + PotentialGDP + DiscountRate2qChange + UNRATE + pres_party + time_to_election + senate_dem_rep + house_dem_rep + WTI_crude_price, model = "normal", data = cpi.data2, cite = FALSE)
+
+# Productivity change
+S5 <- zelig(error.prop.deflator.q2 ~ recession + ExpenditureGDP + PotentialGDP + DiscountRate2qChange + UNRATE + pres_party + time_to_election + senate_dem_rep + house_dem_rep + productivity_change, model = "normal", data = cpi.data2, cite = FALSE)
+
+# Number of interstate conflicts
+S6 <- zelig(error.prop.deflator.q2 ~ recession + ExpenditureGDP + PotentialGDP + DiscountRate2qChange + UNRATE + pres_party + time_to_election + senate_dem_rep + house_dem_rep + num_conflicts, model = "normal", data = cpi.data2, cite = FALSE)
---
+++ Data/DataCreation/DataCreatorPt5.R
@@ -0,0 +1,50 @@
+##############
+# Add PSRM R and R Suggested Variables
+# Christopher Gandrud
+# Updated 13 October 2013
+##############
+
+library(lubridate)
+library(plyr)
+
+# Load main data
+cpi.data <- read.csv("/git_repositories/GreenBook/Data/GB_FRED_cpi_2007.csv")
+cpi.data$year <- gsub("\\.[1-4]", "", cpi.data$Quarter)
+
+# Load Productivity data
+## Data downloaded from the BLS, series ID: PRS85006092
+product <- read.csv("~/Dropbox/GreenBook/Base_Data/BLS_Productivity2013.csv", stringsAsFactors = FALSE)
+
+product$Period <- gsub("Q0", "", product$Period)
+
+product$Quarter <- as.numeric(paste(product$Year, product$Period, sep = "."))
+
+product <- product[, c("Quarter", "Value")]
+names(product) <- c("Quarter", "productivity_change")
+
+# Load Oil Price data
+## Data downloaded from FRED. Symbol OILPRICE (West Texas Intermediate Crude)
+## The FRED interface was used to convert the data into quarter averages and % change from quarter one year before.
+oil <- read.csv("~/Dropbox/GreenBook/Base_Data/WTICrudePriceChange.csv", stringsAsFactors = FALSE)
+oil <- subset(oil, OILPRICE_CH1 != "#N/A")
+oil$OILPRICE_CH1 <- as.numeric(oil$OILPRICE_CH1)
+
+oil$Quarter <- ymd(oil$Quarter)
+oil$Quarter <- quarter(oil$Quarter, with_year = TRUE)
+names(oil) <- c("Quarter", "WTI_crude_price")
+
+# Number of armed conflicts
+## Data downloaded from PCR UU (http://www.pcr.uu.se/research/ucdp/datasets/onset_of_intrastate_armed_conflict/)
+conflict <- read.csv("~/Dropbox/GreenBook/Base_Data/OnsetOfInterStateConflict.csv", stringsAsFactors = FALSE)
+# Keep year and incidencev412, dummy for each country year that there was interstate conflict
+conflict <- conflict[, c(1, 3)]
+conflict <- ddply(conflict, "year", transform, num_conflicts = sum(incidencev412))
+conflict <- conflict[!duplicated(conflict[, c(1, 3)]), ]
+conflict <- conflict[, c("year", "num_conflicts")]
+
+# Combine data sets
+cpi.data <- merge(cpi.data, product, all.x = TRUE)
+cpi.data <- merge(cpi.data, oil, all.x = TRUE)
+cpi.data <- merge(cpi.data, conflict, all.x = TRUE)
+
+write.table(x = cpi.data, file = "/git_repositories/GreenBook/Data/GB_FRED_cpi_2007.csv", sep = ",")
